1fa91305244f1bb0022394f8b7a26325f39468d0
dudamollica <dudarenno@yahoo.com.br>
2022-12-02 22:03:45 -0300

    tela3: ao clicar na seta aparece a resposta

--- src/components/Respostas.js
+++ src/components/Respostas.js
@@ -5,11 +5,68 @@ export default function Respostas(props) {
     const disappear = props.appearAnswer.includes(props.answer)
     return (
         <AnswerBox disappear={disappear}>
-            {props.answer}
+            <span>{props.answer}</span>
+            <BoxButtons>
+                <Red>
+                    Não lembrei
+                </Red>
+                <Orange>
+                    Quase não lembrei
+                </Orange>
+                <Green>
+                    Zap!
+                </Green>
+            </BoxButtons>
         </AnswerBox>
     )
 }
 
 const AnswerBox = styled.div`
-display: ${props => props.disappear?"block":"none"}
+display: ${props => props.disappear ? "flex" : "none"};
+width: 50%;
+height: 131px;
+background: #FFFFD5;
+box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
+border-radius: 5px;
+margin-bottom: 25px;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+span{
+font-family: 'Recursive';
+font-style: normal;
+font-weight: 400;
+font-size: 18px;
+}
+`
+
+const BoxButtons = styled.div`
+    display:flex;
+    margin-top: 23px;
+div{
+    width: 100px;
+    height: 45px;
+    border-radius: 5px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    font-family: 'Recursive';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 15px;
+    color: white;
+}
+`
+const Red = styled.div`
+background: #FF3030;
+margin-right: 10px;
+`
+const Orange = styled.div`
+background: #FF922E;
+margin-right: 10px;
+`
+
+const Green = styled.div`
+background: #2FBE34;
 `
